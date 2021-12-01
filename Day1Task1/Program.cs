c0c0787ee7b63a27081c8e43b4cda0285471a46e
Sergey Savchenko <Sergey.Savchenko@dataart.com>
2021-12-02 01:07:21 +0300

    Day1

---
+++ Day1Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿var s = Console.ReadLine();
+var prev = int.Parse(s);
+var count = 0;
+while(!string.IsNullOrEmpty(s))
+{
+    var next = int.Parse(s);
+    if (next > prev)
+    {
+        count++;
+    }
+    prev = next;
+    s = Console.ReadLine();
+}
+
+Console.WriteLine(count);
---
+++ Day1Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿var mas = new int[4];
+
+for (int i = 1; i < mas.Length; i++)
+{
+    mas[i] = int.Parse(Console.ReadLine());
+}
+
+var s = Console.ReadLine();
+var count = 0;
+
+do
+{
+    for (int i = 0; i < mas.Length - 1; i++)
+    {
+        mas[i] = mas[i + 1];
+    }
+
+    mas[mas.Length - 1] = int.Parse(s);
+
+    if (mas[1] + mas[2] + mas[3] > mas[0] + mas[1] + mas[2])
+    {
+        count++;
+    }
+
+    s = Console.ReadLine();
+} while (!string.IsNullOrEmpty(s));
+
+Console.WriteLine(count);

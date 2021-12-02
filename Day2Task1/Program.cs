1c09568e2ae9ddd1fa674036e15691cbc6b77e84
Sergey Savchenko <Sergey.Savchenko@dataart.com>
2021-12-02 19:38:04 +0300

    Day2

---
+++ Day2Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿var x = 0;
+var y = 0;
+
+var s = Console.ReadLine();
+
+while (!string.IsNullOrEmpty(s))
+{
+    var parts = s.Split(' ');
+    var val = int.Parse(parts[1]);
+    switch (parts[0])
+    {
+        case "forward":
+            x += val;
+            break;
+        case "down":
+            y += val;
+            break;
+        case "up":
+            y -= val;
+            break;
+    }
+
+    s = Console.ReadLine();
+}
+
+Console.WriteLine(x * y);
---
+++ Day2Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿var x = 0;
+var y = 0;
+var aim = 0;
+
+var s = Console.ReadLine();
+
+while (!string.IsNullOrEmpty(s))
+{
+    var parts = s.Split(' ');
+    var val = int.Parse(parts[1]);
+    switch (parts[0])
+    {
+        case "forward":
+            x += val;
+            y += val * aim;
+            break;
+        case "down":
+            aim += val;
+            break;
+        case "up":
+            aim -= val;
+            break;
+    }
+
+    s = Console.ReadLine();
+}
+
+Console.WriteLine(x * y);

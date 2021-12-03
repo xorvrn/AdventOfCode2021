fb57a9a09c779111a2d022f5374e12dda908c3a2
Sergey Savchenko <Sergey.Savchenko@dataart.com>
2021-12-03 13:47:54 +0300

    Day3

---
+++ Day3Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿var s = Console.ReadLine();
+var mas = new int[s.Length];
+
+while (!string.IsNullOrEmpty(s))
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (s[i] == '0')
+        {
+            mas[i]--;
+        }
+        else
+        {
+            mas[i]++;
+        }
+    }
+
+    s = Console.ReadLine();
+}
+
+var gamma = string.Join("", mas.Select(x => x > 0 ? '1' : '0'));
+var epsilon = string.Join("", mas.Select(x => x > 0 ? '0' : '1'));
+
+Console.WriteLine(Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2));
---
+++ Day3Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿var data = new List<string>();
+var s = Console.ReadLine();
+while (!string.IsNullOrEmpty(s))
+{
+    data.Add(s);
+    s = Console.ReadLine();
+}
+
+var o2 = Processor(data, (x, y) => x >= y);
+var co2 = Processor(data, (x, y) => x < y);
+
+string Processor(List<string> data, Func<int, int, bool> comparer)
+{
+    var index = 0;
+    do
+    {
+        var one = data.Where(x => x[index] == '1').ToList();
+        var zero = data.Where(x => x[index] == '0').ToList();
+
+        data = comparer(one.Count, zero.Count) ? one : zero;
+
+        index++;
+    } while (data.Count > 1);
+
+    return data.First();
+}
+
+Console.WriteLine(Convert.ToInt32(o2, 2) * Convert.ToInt32(co2, 2));

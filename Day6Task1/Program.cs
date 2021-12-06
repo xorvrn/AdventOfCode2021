816b56b9b784eacec089c3b3c1426127f77f2c5a
xor <xorvrn@gmail.com>
2021-12-06 18:30:27 +0300

    Day6

---
+++ Day6Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿var s = Console.ReadLine();
+var data = s.Split(',').Select(x => int.Parse(x)).ToList();
+
+for (int d = 0; d < 80; d++)
+{
+    var len = data.Count;
+    for (int i = 0; i < len; i++)
+    {
+        if (data[i] > 0)
+        {
+            data[i]--;
+        }
+        else if (data[i] == 0)
+        {
+            data[i] = 6;
+            data.Add(8);
+        }
+    }
+}
+
+Console.WriteLine(data.Count);
---
+++ Day6Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿var s = Console.ReadLine();
+var startData = s.Split(',').Select(x => byte.Parse(x)).ToList();
+
+var data = new decimal[9];
+for (int i = 0; i < startData.Count; i++)
+{
+    data[startData[i]]++;
+}
+
+for (int day = 0; day < 256; day++)
+{
+    var newFish = data[0];
+
+    for (int i = 1; i < 9; i++)
+    {
+        data[i - 1] = data[i];
+    }
+    data[6] += newFish;
+    data[8] = newFish;
+
+}
+
+Console.WriteLine(data.Sum());

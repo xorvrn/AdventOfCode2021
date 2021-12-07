6cdbbc0beb5f7d63197173101a2e84451e405dee
xor <xorvrn@gmail.com>
2021-12-07 15:12:49 +0300

    Day7

---
+++ Day7Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿var s = Console.ReadLine();
+var positions = s.Split(',')
+    .Select(int.Parse)
+    .GroupBy(x => x)
+    .ToDictionary(x => x.Key, x => x.Count());
+
+var min = int.MaxValue;
+
+foreach (var position in positions)
+{
+    var localMin = 0;
+    foreach (var position2 in positions)
+    {
+        localMin += Math.Abs(position2.Key - position.Key) * position2.Value;
+    }
+
+    if (localMin < min)
+    {
+        min = localMin;
+    }
+}
+
+Console.WriteLine(min);
---
+++ Day7Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿var s = Console.ReadLine();
+var positions = s.Split(',')
+    .Select(int.Parse)
+    .GroupBy(x => x)
+    .ToDictionary(x => x.Key, x => x.Count());
+
+var minPosition = positions.Keys.Min();
+var maxPosition = positions.Keys.Max();
+
+var costs = new int[maxPosition - minPosition + 1];
+var cost = 0;
+for (var i = 0; i < costs.Length; i++)
+{
+    cost += i;
+    costs[i] = cost;
+}
+
+var min = int.MaxValue;
+
+for (int p = minPosition; p <= maxPosition; p++)
+{
+    var localMin = 0;
+    foreach (var position2 in positions)
+    {
+        localMin += costs[Math.Abs(position2.Key - p)] * position2.Value;
+    }
+
+    if (localMin < min)
+    {
+        min = localMin;
+    }
+}
+
+Console.WriteLine(min);

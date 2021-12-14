af12e6a64461ef287398b1e07e05c1c1d4db9b0e
xor <xorvrn@gmail.com>
2021-12-15 01:15:03 +0300

    Day14

---
+++ Day14Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+var lines = File.ReadAllLines("input.txt");
+
+var input = new StringBuilder(lines[0]);
+
+var rules = new Dictionary<string, string>();
+{
+    for (int i = 2; i < lines.Length; i++)
+    {
+        var parts = lines[i].Split(" -> ");
+        rules.Add(parts[0], parts[1]);
+    }
+}
+
+for (int i = 0; i < 10; i++)
+{
+    var index = 0;
+    while (index < input.Length - 1)
+    {
+        var pair = input[index].ToString() + input[index + 1];
+        if (rules.TryGetValue(pair, out var element))
+        {
+            input.Insert(index + 1, element);
+            index++;
+        }
+        index++;
+    }
+    Console.WriteLine(i);
+}
+
+var groups = input.ToString().GroupBy(x => x);
+Console.WriteLine(groups.Max(x => x.Count()) - groups.Min(x => x.Count()));
---
+++ Day14Task2/Program.cs
@@ -0,0 +1,66 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+var input = lines[0];
+
+var rules = new Dictionary<string, string>();
+{
+    for (int i = 2; i < lines.Length; i++)
+    {
+        var parts = lines[i].Split(" -> ");
+        rules.Add(parts[0], parts[1]);
+    }
+}
+
+var elements = new Dictionary<string, ulong>();
+for (int i = 0; i < input.Length; i++)
+{
+    SetValue(elements, $"{input[i]}", 1);
+}
+
+var pairs = new Dictionary<string, ulong>();
+for (int i = 0; i < input.Length - 1; i++)
+{
+    var pair = input[i].ToString() + input[i + 1];
+    SetValue(pairs, pair, 1);
+}
+
+for (int i = 0; i < 40; i++)
+{
+    var tmp = new Dictionary<string, ulong>();
+
+    foreach (var pair in pairs)
+    {
+        if (rules.TryGetValue(pair.Key, out var element))
+        {
+            SetValue(elements, element, pair.Value);
+
+            SetValue(tmp, $"{pair.Key[0]}{element}", pair.Value);
+            SetValue(tmp, $"{element}{pair.Key[1]}", pair.Value);
+        }
+        else
+        {
+            SetValue(tmp, pair.Key, pair.Value);
+        }
+    }
+
+    pairs = tmp;
+}
+
+foreach (var element in elements)
+{
+    Console.WriteLine($"{element.Key} - {element.Value}");
+}
+
+Console.WriteLine(elements.Max(x => x.Value) - elements.Min(x => x.Value));
+
+void SetValue(IDictionary<string, ulong> dict, string key, ulong val)
+{
+    if (dict.ContainsKey(key))
+    {
+        dict[key] += val;
+    }
+    else
+    {
+        dict.Add(key, val);
+    }
+}
